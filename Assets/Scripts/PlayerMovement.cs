7c9267a64d8fb5bee5f9d41ed6b88507e8828c20
unknown <mckeld3@student.op.ac.nz>
2024-02-28 21:58:19 +1300

    ADD: rewrote player movement code

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
+    public float moveSpeed = 5f;
+    public float acceleration;
+    public float decceleration;
+    public float velPower;
     public Vector2 moveInput;
     private Rigidbody2D rb;
+    public float gravityStrength = 9.8f; 
+    
+    public float movement;
 
     [SerializeField]
-    private InputActionReference movement, jump;
+    private InputActionReference movementInput, jump;
+
+
 
     void Start()
     {
@@ -24,9 +32,34 @@ public class PlayerMovement : MonoBehaviour
 
         //Vector2 movement = new Vector2(horizontalInput, verticalInput);
 
-        moveInput = movement.action.ReadValue<Vector2>();
+        moveInput = movementInput.action.ReadValue<Vector2>();
 
-        rb.velocity = moveInput * speed;
+        //rb.velocity = moveInput * speed;
+    }
+
+    void FixedUpdate()
+    {
+        ApplyGravity();
+
+        float targetSpeed = moveInput.x * moveSpeed;
+
+        float speedDif = targetSpeed - rb.velocity.x;
+
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : decceleration;
+
+        float movement = speedDif * accelRate;
+
+        rb.AddForce(movement * Vector2.right);
+
+    }
+
+    void ApplyGravity()
+    {
+        Vector2 gravityForce = Vector2.down * gravityStrength;
+
+        rb.velocity += gravityForce * Time.fixedDeltaTime;
+
+    
     }
 
     /*
