198f498d7d6432e371dbb29ded55608024dc093e
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-03-24 17:52:46 +1300

    feat: powerups reset with player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,9 @@ public class GameManager : MonoBehaviour
     // singleton pattern
     public static GameManager instance;
 
+    // array of game objects that will be reset
+    public GameObject[] powerups;
+
     void Awake()
     {
         if (instance == null)
@@ -32,6 +35,10 @@ public class GameManager : MonoBehaviour
     {
         // store the player's initial position
         initialPosition = player.transform.position;
+
+        powerups = GameObject.FindGameObjectsWithTag("Powerup");
+        // log powerups
+        Debug.Log("Powerups: " + powerups.Length);
     }
 
     // Update is called once per frame
@@ -48,5 +55,14 @@ public class GameManager : MonoBehaviour
         player.GetComponent<ShootController>().ResetCooldown();
         // reset movement
         player.GetComponent<PlayerMovement>().ResetMovement();
+
+        // find all objects in the scene with the tag "Powerup"
+        // if inactivated, activate them
+        
+        foreach (GameObject powerup in powerups)
+        {
+            powerup.SetActive(true);
+        }
+
     }
 }
