53f6686f198890b5708a2942925de29254d3f1e0
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2024-03-25 16:32:18 +1300

    docs: add comments

--- Assets/Scripts/Player/CrosshairMovement.cs
+++ Assets/Scripts/Player/CrosshairMovement.cs
@@ -1,25 +1,43 @@
+/*
+ * File: CrosshairMovement.cs
+ * -------------------------
+ * This file contains the implementation of the crosshair movement.
+ *
+ * Author: Johnathan
+ * Contributions: Assisted by GitHub Copilot, MoreBBlakeyyy(https://www.youtube.com/watch?v=-bkmPm_Besk)
+ */
+ 
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 /// <summary>
 /// This script makes the crosshair follow the mouse position.
-/// Source: https://www.youtube.com/watch?v=-bkmPm_Besk
 /// </summary>
 public class CrosshairMovement : MonoBehaviour
 {
     private Camera mainCam;
     private Vector3 mousePos;
 
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
     void Start()
     {
         mainCam = Camera.main;
     }
 
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
     void Update()
     {
+        // Get the current position of the mouse cursor in world coordinates
         mousePos = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        // Calculate a Vector3 that points from the current position of the object to the position of the mouse cursor 
         Vector3 rotation = mousePos - transform.position;
+        // Calculate the angle of rotation needed to make the object face the mouse cursor
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        // Apply the rotation to the object
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
     }
 }
--- Assets/Scripts/Player/RotateParticleWithObject.cs
+++ Assets/Scripts/Player/RotateParticleWithObject.cs
@@ -1,29 +1,44 @@
+/*
+ * File: RotateParticleWithObject.cs
+ * -------------------------
+ * This file contains the implementation of the particle system rotation with a target object.
+ *
+ * Author: Johnathan
+ * Contributions: Assisted by GitHub Copilot
+ */
+
 using UnityEngine;
 
 /// <summary>
 /// This script makes the Particle System rotate with a target object.
-/// This is to be used for the shotgun particles to rotate with the crosshair.
+/// This is used for the shotgun particles to rotate with the crosshair.
 /// </summary>
-
 public class RotateParticleWithObject : MonoBehaviour
 {
-    public Transform targetObject; // The object whose rotation you want to match
-    private ParticleSystem particleSystem; // Reference to the Particle System
-    public Vector3 rotationOffset; // Offset to apply to the rotation
+    public Transform TargetObject; // The object whose rotation you want to match
+    public Vector3 RotationOffset; // Offset to apply to the rotation
 
-    void Start()
+    private ParticleSystem particleSystem; // Reference to the Particle System
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    private void Start()
     {
         // Get the Particle System component
         particleSystem = GetComponent<ParticleSystem>();
     }
 
-    void Update()
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    private void Update()
     {
         // Check if the target object is not null and the Particle System is valid
-        if (targetObject != null && particleSystem != null)
+        if (TargetObject != null && particleSystem != null)
         {
             // Match the rotation of the Particle System with the rotation of the target object
-            Quaternion targetRotation = targetObject.rotation * Quaternion.Euler(rotationOffset);
+            Quaternion targetRotation = TargetObject.rotation * Quaternion.Euler(RotationOffset);
             transform.rotation = targetRotation;
         }
     }
