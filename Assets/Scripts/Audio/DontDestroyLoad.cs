a5efca4ee710c2b86a3b650b3b317d4a73059805
unknown <mckeld3@student.op.ac.nz>
2024-04-26 18:11:11 +1200

    ADD: music now persists through loads

---
+++ Assets/Scripts/Audio/DontDestroyLoad.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <remarks>
+/// Code taken from:
+/// https://www.youtube.com/watch?v=Xtfe5S9n4SI
+/// </remarks>
+
+/// <summary>
+/// 
+/// </summary>
+public class DontDestroyLoad : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] musicObject = GameObject.FindGameObjectsWithTag("Music");
+        if(musicObject.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
